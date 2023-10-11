8b24d73c3d455b4b1058d09f4c2d03829e9fa44f
karko13 <145532713+karko13@users.noreply.github.com>
2023-10-11 08:20:52 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TrackBarKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.track = new System.Windows.Forms.TrackBar();
+            this.mjau = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.track)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // track
+            // 
+            this.track.Location = new System.Drawing.Point(170, 47);
+            this.track.Maximum = 100;
+            this.track.Name = "track";
+            this.track.Size = new System.Drawing.Size(390, 45);
+            this.track.TabIndex = 0;
+            this.track.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // mjau
+            // 
+            this.mjau.Location = new System.Drawing.Point(304, 176);
+            this.mjau.Name = "mjau";
+            this.mjau.Size = new System.Drawing.Size(100, 20);
+            this.mjau.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TrackBox:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mjau);
+            this.Controls.Add(this.track);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.track)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar track;
+        private System.Windows.Forms.TextBox mjau;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
